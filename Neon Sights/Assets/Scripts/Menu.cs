3975bcf68feb361bc66dbe4184562db972f27ceb
ewatkins2010 <eddiewattz@gmail.com>
2015-09-17 15:04:29 -0400

    Getting Started

---
+++ Neon Sights/Assets/Scripts/Menu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour {
+	public List<GameObject> screens;
+	public EventSystem selection;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < screens.Count; i++) {
+			if (screens[i].gameObject.name != "Main")
+				screens[i].SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChooseLevel(int lvl){
+		Application.LoadLevel (lvl);
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+
+	public void ChangeScreens(string label){
+		for (int i = 0; i < screens.Count; i++) {
+			if (screens[i].gameObject.name != label)
+				screens[i].SetActive (false);
+			else
+				screens[i].SetActive (true);
+		}
+	}
+
+}
