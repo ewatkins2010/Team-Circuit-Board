b9d10707dbc21c79f666c1d7c5c977cca2825f15
KatieBolster <katie.bolster@yahoo.com>
2015-09-20 14:00:59 -0400

    Enemy AI
    Enemy shoots at player, I put in basic Muzzle Flash for conveyance

---
+++ Neon Sights/Assets/Scripts/Enemy.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+    public float spreadFactor;
+    public float delayTime = .2f;
+    private float counter = 0;
+    public Renderer muzzleFlash;
+    
+
+
+	
+	void Start () {
+
+        //hides muzzleflash 
+        muzzleFlash.enabled = false;
+	
+	}
+	
+
+
+	
+	void Update () {
+
+        //delay on shooting
+        counter += Time.deltaTime;
+
+        if(counter > delayTime)
+        {
+            attack();
+            counter = 0;
+        }
+        
+
+	}
+
+
+
+    void attack() {
+
+        //enemy attacks have spread and aren't 100% accurate
+        Vector3 direction = transform.forward;
+        direction.x += Random.Range(-spreadFactor, spreadFactor);
+        direction.y += Random.Range(-spreadFactor, spreadFactor);
+        direction.z += Random.Range(-spreadFactor, spreadFactor);
+
+
+        //if attack hits player, pleayer health goes down
+        RaycastHit hit;
+        if(Physics.Raycast (transform.position, direction, out hit))
+        {
+            if(hit.collider.gameObject.tag == "Player")
+            {
+                hit.collider.gameObject.GetComponent<HealthScript>().health -= 5f;
+            }
+
+            //show muzzle flash when shooting
+            StartCoroutine("MuzzleFlash");
+        }
+    }
+
+
+    //muzzle flash 
+    IEnumerator MuzzleFlash() {
+
+        muzzleFlash.enabled = true;
+        yield return new WaitForSeconds(.2f);
+        muzzleFlash.enabled = false;
+
+        
+        
+
+    }
+}
---
+++ Neon Sights/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+    public float health;
+    //public static bool isPlayerAlive = true;
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+	
+        if(health < 0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
