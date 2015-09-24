28d08ea856c4ef1307525ca08875f2828f4ba568
ewatkins2010 <eddiewattz@gmail.com>
2015-09-24 10:54:41 -0400

    Updated Assets

---
+++ Tri Towers/Assets/Scripts/PlayerStas.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStas : MonoBehaviour {
+	public int ammo;
+
+	// Use this for initialization
+	void Start () {
+		ammo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
