6d6203dcab76cafe741b026d4dc533905e41b973
CROxley <cro1014@knights.ucf.edu>
2015-09-27 16:34:35 -0400

    Put in Ed's additions
    weapon swapping
    reticles
    sound, etc.

---
+++ Tri Towers/Assets/Scripts/IncomingFire.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncomingFire : MonoBehaviour 
+{
+	public float speed = 1.0f;
+	public bool gibOnCollision = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += transform.forward * speed * Time.deltaTime;
+
+	}
+
+	void OnCollisionEnter()
+	{
+		if(gibOnCollision)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
--- Tri Towers/Assets/Scripts/TurretFireAfterDelay.cs
+++ Tri Towers/Assets/Scripts/TurretFireAfterDelay.cs
@@ -1,35 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShootAfterDelay : MonoBehaviour {
-	
+public class TurretFireAfterDelay : MonoBehaviour {
+
 	public float delay = 1.0f;
 	public GameObject e_Bullet;
-	
-	private GameObject player;
-	
+
 	private Animator animator;
-	
+
 	void Start () 
 	{
-		player = GameObject.FindGameObjectWithTag("Player");
 		animator = GetComponentInChildren<Animator>();
 		Invoke ("Shoot", delay);
 	}
-	
+
 	void LateUpdate()
 	{
 		animator.SetBool ("Shoot", false);
 	}
-	
+
 	void Shoot () 
 	{
 		//Deal damage to player (TBA)
-		Instantiate(e_Bullet, transform.position,Quaternion.LookRotation(Camera.main.transform.position - transform.position));
-		
+
 		//Play animation
 		animator.SetBool ("Shoot", true);
-		
+
 		Invoke ("Shoot", delay);
 	}
 }
