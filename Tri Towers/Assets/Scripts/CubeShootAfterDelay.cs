01b326017a0c5151a1e0564b296a01076f3da945
CROxley <cro1014@knights.ucf.edu>
2015-09-28 01:36:37 -0400

    Chris's final prototype adjustments
    Ok Ed. It's all yours. Sorry it took this long.

---
+++ Tri Towers/Assets/Scripts/CubeShootAfterDelay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeShootAfterDelay : MonoBehaviour {
+	
+	public float delay = 1.0f;
+	public GameObject e_Bullet;
+	public Transform shootPoint;
+	
+	private GameObject player;
+	
+	private Animator animator;
+	
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		animator = GetComponentInChildren<Animator>();
+		Invoke ("Shoot", delay);
+	}
+	
+	void LateUpdate()
+	{
+		animator.SetBool ("Shoot", false);
+	}
+	
+	void Shoot () 
+	{
+		//Deal damage to player (TBA)
+		Instantiate(e_Bullet, transform.position,Quaternion.LookRotation(Camera.main.transform.position - shootPoint.position));
+		
+		//Play animation
+		animator.SetBool ("Shoot", true);
+		
+		Invoke ("Shoot", delay);
+	}
+}
