9b7034d569cce99abeb9658e25af278c731b9c2f
CROxley <cro1014@knights.ucf.edu>
2015-10-13 02:43:40 -0400

    Update
    I've been working in the Prototype scene. I've got an enemy moving but
    it's far from even being remotely functional. I could definitely use a
    hand.

---
+++ Tri Towers/Assets/Scripts/Patrol Scripts/EnemyPatrol.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	public Transform[] Waypoints;
+	public float Speed;
+	public int curWayPoint;
+	public bool doPatrol = true;
+	public Vector3 Target;
+	public Vector3 MoveDirection;
+	public Vector3 Velocity;
+	public bool initialRotation;
+
+	private GameObject player;
+
+	void Update () {
+
+		if(curWayPoint < Waypoints.Length)
+		{
+			Target = Waypoints[curWayPoint].position;
+			MoveDirection = Target - transform.position;
+			Velocity = GetComponent<Rigidbody>().velocity;
+
+			if(MoveDirection.magnitude < 1)
+				curWayPoint++;
+
+			else
+				Velocity = MoveDirection.normalized * Speed;
+
+		}
+		else
+		{
+			if(doPatrol)
+				doPatrol = false;
+
+			else
+				Velocity = Vector3.zero;
+		}
+
+		GetComponent<Rigidbody>().velocity = Velocity;
+		transform.LookAt (Target);
+	}
+}
---
+++ Tri Towers/Assets/Scripts/Patrol Scripts/Waypoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour {
+
+	public Waypoints nextWaypoint;
+	
+	void OnDrawGizmos () 
+	{
+		if (nextWaypoint != null) 
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine (transform.position, nextWaypoint.transform.position);
+		}
+	}
+}
