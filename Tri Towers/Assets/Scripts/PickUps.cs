a42212688c4ea73e98ec3c5d5d1528d35cb27195
ewatkins2010 <eddiewattz@gmail.com>
2015-10-14 00:34:38 -0400

    Multiplayer Functionality

--- Tri Towers/Assets/Scripts/PickUps.cs
+++ Tri Towers/Assets/Scripts/PickUps.cs
@@ -4,11 +4,8 @@ using System.Collections;
 public class PickUps : MonoBehaviour {
 	//enter item number in the inspector
 	public int itemNumber;
-
-	//drag whatever has the shooting script and the health script into these fields in the inspector
+	
 	//drag the AudioSource of whatever sound you want the pickup to make when picked up
-	public HealthScript h;
-	public Shoot s;
 	public AudioSource a;
 
 
@@ -28,17 +25,17 @@ public class PickUps : MonoBehaviour {
 			switch (itemNumber){
 			//this is the health pickup
 			case 1:
-				h.health += 10;
+				col.gameObject.GetComponentInParent<HealthScript>().health += 10;
 				break;
 			//this is the rifle pickup. giving the player 50 shots for now but we can always change later
 			case 2:
-				s.ammo = 50;
-				s.gunType = 1;
+				col.gameObject.GetComponentInParent<Shoot>().ammo = 50;
+				col.gameObject.GetComponentInParent<Shoot>().gunType = 1;
 				break;
 			//this is the shot gun. around 5 bullets is pretty standard I think
 			case 3:
-				s.ammo = 5;
-				s.gunType = 2;
+				col.gameObject.GetComponentInParent<Shoot>().ammo = 5;
+				col.gameObject.GetComponentInParent<Shoot>().gunType = 2;
 				break;
 			default:
 				break;
