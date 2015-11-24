e32ea78da99081c10e2744ccb119d377c1bcd957
kopyright13 <Vault@Hezekiahs-MacBook-Pro.local>
2015-11-23 21:45:08 -0500

    Level1

---
+++ Tri Towers/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+
+	public Animator Door;
+	void Start(){
+		Door.enabled = false;
+	}
+	void OnTriggerEnter(Collider Other)
+	{
+		if(Other.gameObject.tag == "Player")
+		{
+			Door.enabled = true;
+		}
+	}
+}
---
+++ Tri Towers/Assets/Scripts/Patrol Scripts/TurretPatrol.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretPatrol : MonoBehaviour {
+	public Transform[] Waypoints;
+	public float Speed;
+	public int curWayPoint;
+	public bool doPatrol;
+	public Vector3 Target;
+	public Vector3 MoveDirection;
+	public Vector3 Velocity;
+	public bool initialRotation;
+	public Animator anim;
+	private GameObject player;
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+
+		if(curWayPoint < Waypoints.Length & doPatrol)
+		{
+			Target = Waypoints[curWayPoint].position;
+			MoveDirection = Target - transform.position;
+			Velocity = GetComponent<Rigidbody>().velocity;
+
+			if(MoveDirection.magnitude < 1)
+				curWayPoint++;
+			else{
+				Velocity = MoveDirection.normalized * Speed;
+				anim.SetBool ("Fire", true);
+			}
+
+		}
+		else
+		{
+			if(doPatrol)
+				doPatrol = false;
+			else {
+				Velocity = Vector3.zero;
+				Target = player.transform.position;
+				anim.SetBool ("Fire", false);
+			}
+		}
+
+		GetComponent<Rigidbody>().velocity = Velocity;
+		transform.LookAt (Target);
+	}
+}
